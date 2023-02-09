ff06f4dd3d40a06b68261019cd502c0070b88345
Vince489 <vincentcornelius489@gmail.com>
2023-02-08 19:01:52 -0500

    route

--- server/api/bob.js
+++ server/api/bob.js
@@ -1,7 +1,14 @@
-export default defineEventHandler(() => {
+export default defineEventHandler(async(event) => {
+
+  // const { name } = getQuery(event)
+
+  // const { age } = await readBody(event)
+
+  // api call w/o private key
+  const { data } = await $fetch('https://api.currencyapi.com/v3/latest?apikey=ZhWAdT7KkWgKujtKJtvKWSc5AOybXAbnnylgrofo')
 
   return {
-    message: `Hello`
+    data
   }
 
 })
